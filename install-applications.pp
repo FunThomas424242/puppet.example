a1b54161c468411f943a1691ab47a0838c297a9b
Thomas Schubert <funthomas424242@gmail.com>
2014-11-17 20:53:05 +0100

    deps fixed

--- install-applications.pp
+++ install-applications.pp
@@ -17,7 +17,7 @@ class system-update {
 
 include system-update
 
-class grundsystem::params {
+class applications::params {
   case $::osfamily {
     "Debian": {
       case $::lsbdistcodename {
@@ -33,35 +33,26 @@ class grundsystem::params {
 }
 
 
-class grundsystem inherits grundsystem::params {
-  
+class applications inherits applications::params {
 	
-	$libsToInstall = ["hplip-gui","librarian-puppet","puppet", "xbmc", "qasconfig" ,"libasound2-plugin-equal","alsa-base","software-properties-gtk", "aptitude"]
-	  package { $libsToInstall:
+	 $packagesToInstall = [ "dropbox", "blogilo","eclipse-platform", "giggle", "jabref", "kiki", "freemind", "spim", "simple-scan", "evolution", "keepass2", "vlc", "electrum","calibre","gramps","blender","gimp","firefox","emacs24","alsaplayer", "alsa-utils","flush", "gpa"]
+	  package { $packagesToInstall:
 	    ensure => "latest",
 	    require => Exec['apt-get update']
 	  }
-	
-	$tools = ["p7zip", "fglrx", "padevchooser", "torchat", "gns3", "xscreensaver", "ksnapshot"]
-	  package { $tools:
-	    ensure => "latest",
-	    require => Exec['apt-get update']
-	  }
-	
-	
+
 	$officePackages = ["libreoffice","libreoffice-base","libreoffice-l10n-de","libreoffice-draw","libreoffice-calc","libreoffice-writer", "libreoffice-math", "libreoffice-impress" ]
 	  package { $officePackages:
 	    ensure => "latest",
 	    require => Exec['apt-get update']
 	  }
+
 	
-	
-	 $packagesToInstall = [ "blogilo","eclipse-platform", "giggle", "jabref", "kiki", "freemind", "spim", "simple-scan", "evolution", "keepass2", "vlc", "electrum","calibre","gramps","blender","gimp","firefox","emacs24","alsaplayer", "alsa-utils","flush", "gpa","dropbox", "nautilus-dropbox"]
-	  package { $packagesToInstall:
+	$gamesToInstall = [ "fltk1.1-games", "cgoban", "gnubg", "lincity-ng" ]
+	  package { $gamesToInstall:
 	    ensure => "latest",
 	    require => Exec['apt-get update']
 	  }
-	
 	
 	$packagesToPurge = ["gnumeric", "rutilt", "etherape", "gnome-mplayer", "sylpheed","transmission-gtk","transmission-remote-gtk","transmission-qt","transgui", "abiword"]
 	  package { $packagesToPurge:
@@ -74,5 +65,5 @@ class grundsystem inherits grundsystem::params {
 
 
 node default {
-	include grundsystem
+	include applications
 }
--- install-grundsystem.pp
+++ install-grundsystem.pp
@@ -36,40 +36,20 @@ class grundsystem::params {
 class grundsystem inherits grundsystem::params {
   
 	
-	$libsToInstall = ["network-manager-gnome","hplip-gui","librarian-puppet","puppet", "xbmc", "qasconfig" ,"libasound2-plugin-equal","alsa-base","software-properties-gtk", "aptitude"]
+	$libsToInstall = ["network-manager","network-manager-gnome","hplip-gui","librarian-puppet","puppet", "qasconfig" ,"libasound2-plugin-equal","alsa-base","software-properties-gtk", "aptitude"]
 	  package { $libsToInstall:
 	    ensure => "latest",
 	    require => Exec['apt-get update']
 	  }
 	
-	$tools = ["p7zip", "fglrx", "padevchooser", "torchat", "gns3", "xscreensaver", "ksnapshot"]
+	$tools = ["kwalletmanager","p7zip", "fglrx", "padevchooser", "torchat", "gns3", "xscreensaver", "ksnapshot"]
 	  package { $tools:
 	    ensure => "latest",
 	    require => Exec['apt-get update']
 	  }
 	
 	
-	$officePackages = ["libreoffice","libreoffice-base","libreoffice-l10n-de","libreoffice-draw","libreoffice-calc","libreoffice-writer", "libreoffice-math", "libreoffice-impress" ]
-	  package { $officePackages:
-	    ensure => "latest",
-	    require => Exec['apt-get update']
-	  }
-	
-	
-	 $packagesToInstall = [ "eclipse-platform", "giggle", "jabref", "kiki", "freemind", "spim", "simple-scan", "evolution", "keepass2", "vlc", "electrum","calibre","gramps","blender","gimp","firefox","emacs24","alsaplayer", "alsa-utils","flush", "gpa"]
-	  package { $packagesToInstall:
-	    ensure => "latest",
-	    require => Exec['apt-get update']
-	  }
-	
-         $gamesToInstall = [ "fltk1.1-games", "cgoban", "gnubg", "lincity-ng" ]
-	  package { $gamesToInstall:
-	    ensure => "latest",
-	    require => Exec['apt-get update']
-	  }
-
-	
-	$packagesToPurge = ["gnumeric", "rutilt", "etherape", "gnome-mplayer", "nautilus-dropbox",  "sylpheed","transmission-gtk","transmission-remote-gtk","transmission-qt","transgui", "abiword"]
+	$packagesToPurge = ["gnumeric", "rutilt", "etherape", "gnome-mplayer",  "sylpheed","transmission-gtk","transmission-remote-gtk","transmission-qt","transgui", "abiword"]
 	  package { $packagesToPurge:
 	    ensure => "absent",
 	    require => Exec['apt-get update']
