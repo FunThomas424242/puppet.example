14ceeb04352c7cb6639cd58ebe7abcca8b114280
Thomas Schubert <funthomas424242@gmail.com>
2015-05-14 21:12:46 +0200

    changed maven install the latest version
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- maven-install.pp
+++ maven-install.pp
@@ -1,10 +1,16 @@
-# save current vars
-file { "/tmp/facts.yaml":
-    content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"),
-}
 
-maven { "/tmp/maven-core-3.1.1.jar":
-  id => "org.apache.maven:maven-core:3.1.1:jar",
+#maven { "/tmp/maven-core-3.1.1.jar":
+#  groupid => "org.apache.maven",
+#  artifactid => "maven-core",
+#  version => "3.1.1",
+#  packaging => "jar",
+#  classifier => "",
+#  repos => ["http://mvnrepository.com/artifact/"],
+#  ensure => "present",
+#}
+
+maven { "/tmp/maven-core-3.2.5.jar":
+  id => "org.apache.maven:maven-core:3.2.5:jar",
   repos => ["central::default::http://repo.maven.apache.org/maven2","http://mirrors.ibiblio.org/pub/mirrors/maven2"],
 }
 
