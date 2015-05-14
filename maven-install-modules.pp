536a032865d09b4ad327e4b7428d3ffcd2fcb5e0
Thomas Schubert <funthomas424242@gmail.com>
2015-05-14 20:39:48 +0200

    maven fixed
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

--- maven-install-modules.pp
+++ maven-install-modules.pp
@@ -1,11 +1,27 @@
-# wget 
-exec { "masestrodev-wget-modul":
+
+# stdlib 
+exec { "stdlib":
     command => "sudo puppet module install --force maestrodev-wget",
     path    => "/usr/local/bin/:/bin/:/usr/bin",
 }
 
-# maven 
-exec { "maven-modul":
+
+# maven Maestrodev
+exec { "maven-maestrodev":
     command => "sudo puppet module install --force maestrodev-maven",
     path    => "/usr/local/bin/:/bin/:/usr/bin",
 }
+
+
+# maven BoxUpp
+exec { "maven-boxupp":
+    command => "sudo puppet module uninstall BoxUpp-apache_maven",
+    path    => "/usr/local/bin/:/bin/:/usr/bin",
+}
+
+# maven UniBet
+exec { "maven-unibet":
+    command => "sudo puppet module uninstall unibet-maven",
+    path    => "/usr/local/bin/:/bin/:/usr/bin",
+}
+
--- maven-uninstall.pp
+++ maven-uninstall.pp
@@ -2,20 +2,9 @@
 file { "/tmp/facts.yaml": content => inline_template("<%= scope.to_hash.reject { |k,v| !( k.is_a?(String) && v.is_a?(String) ) }.to_yaml %>"
   ), }
 
-# maven { "/tmp/maven-core-3.1.1.jar":
-#  id => "org.apache.maven:maven-core:3.1.1:jar",
-#  repos => ["central::default::http://repo.maven.apache.org/maven2","http://mirrors.ibiblio.org/pub/mirrors/maven2"],
-#}
 
-maven { "/tmp/maven-core-3.1.1.jar":
-  groupid    => "org.apache.maven",
-  artifactid => "maven-core",
-  version    => "3.1.1",
-  ensure     => 'purge',
-#  classifier => "sources",
-}
 
 node default {
-  include maven
-  # notice('Well done!')
+  #include maven
+  notice('Nicht möglich :(')
 }
