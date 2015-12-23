eafa680a90edb5b77a63e05f5b15e21d058ab029
huluvu424242 <huluvu424242@stachel.de>
2015-12-23 22:06:07 +0100

    java installation festgelegt auf jdk8u662 2

--- java-install.pp
+++ java-install.pp
@@ -1,14 +1,25 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
+class grundsystem {
+  
+	
+	$libsToInstall = ["libjline-java", "libjava-gnome-java"]
+	  package { $libsToInstall:
+	    ensure => "latest"
+	  }
+
+}
+
+
 class { 'oraclejava::jdk8' :
-#  java           => 'jdk-8u11',
+  java           => 'jdk-8u66',
   java_loc        => '/opt/java',
-#  java_dir        => 'jdk1.8.0_11',
-#  download_url    => 'http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz'     
+  java_dir        => 'jdk1.8.0_66',
+  download_url    => 'http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.tar.gz'
 } 
 
 
 
-#node default {
-#  include oraclejava
-#}
+node default {
+  #include oraclejava
+}
